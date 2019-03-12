361515fcf2c3f68eaf967e613f9dd459c915ce65
makimov <daggerito@gmail.com>
2019-03-12 15:38:20 +0300

    Swagger specification created.

---
+++ main.py
@@ -0,0 +1,21 @@
+from aiohttp import web
+
+
+async def handler(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = 'Hello, ' + name
+    return web.Response(text=text)
+
+
+app = web.Application()
+app.router.add_route('GET', '/', handler)
+app.router.add_route('GET', '/{name}', handler)
+# app.router.add_route('GET', '/company',)
+# app.router.add_route('GET', '/company/{company_id}',)
+# app.router.add_route('POST', '/company/add',)
+# app.router.add_route('POST', '/employee/add')
+# app.router.add_route('PUT', '/employee/assign')
+# app.router.add_route('POST', '/company/goods/add')
+# app.router.add_route('PUT', '/company/goods/assign')
+
+web.run_app(app)
